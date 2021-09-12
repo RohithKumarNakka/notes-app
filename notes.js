9d8105c5b5af7bb5ca0489820194d80d3a6d8d7d
rohith <nakkarohithkumar@gmail.com>
2021-09-12 17:20:13 +0530

    first commit

---
+++ notes.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+
+    saveNotes(notes);
+    console.log(chalk.green.inverse("New note added!"));
+  } else {
+    console.log(chalk.red.inverse("Note title taken!"));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesTokeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesTokeep.length) {
+    console.log(chalk.green.inverse("Note removed!"));
+  } else {
+    console.log(chalk.red.inverse("No note found!"));
+  }
+
+  saveNotes(notesTokeep);
+};
+
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your notes"));
+
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJson);
+};
+
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
+};
